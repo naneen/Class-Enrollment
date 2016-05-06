8bdf896bcb1f11a1f073051aeb8d19874801f5ef
naneen <neen_tta@hotmail.com>
2016-05-06 19:47:17 +0700

    Implemented AngularJS

--- assets/js/getJSON.js
+++ assets/js/getJSON.js
@@ -1,12 +1,12 @@
-// var App = angular.module('App', []);
-//
-// App.controller('CEnrollCtrl', function($scope, $http) {
-// $http.get('https://whsatku.github.io/skecourses/combined.json')
-// // $http.get('subjects/combined.json')
-//      .then(function(res){
-//         $scope.courses = res.data;
-//       });
-// });
+var App = angular.module('App', []);
+
+App.controller('CEnrollCtrl', function($scope, $http) {
+$http.get('https://whsatku.github.io/skecourses/combined.json')
+// $http.get('subjects/combined.json')
+     .then(function(res){
+        $scope.courses = res.data;
+      });
+});
 
 
 // $(document).ready(function() {
