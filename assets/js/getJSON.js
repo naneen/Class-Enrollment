a344765b3621f29c35e386999aafff4b2e5c663f
naneen <neen_tta@hotmail.com>
2016-05-07 21:16:09 +0700

    Able to search & modify js code

--- assets/js/getJSON.js
+++ assets/js/getJSON.js
@@ -1,69 +1,20 @@
-var App = angular.module('App', ['angular-toArrayFilter']);
-
-App.controller('CEnrollCtrl', function($scope, $http) {
-  $http.get('https://whsatku.github.io/skecourses/combined.json')
-  // $http.get('subjects/combined.json')
-    .then(function(res){
-      console.log(res.data);
-      $scope.courses = res.data;
-    });
-  // var lists = $.parseJSON('[' + courses + ']');
-  // console.log(lists);
-});
-
-// App.controller('CEnrollCtrl', function($scope) {
-//     $scope.names = [
-//         'Jani',
-//         'Carl',
-//         'Margareth',
-//         'Hege',
-//         'Joe',
-//         'Gustav',
-//         'Birgit',
-//         'Mary',
-//         'Kai'
-//     ];
+// var App = angular.module('App', ['angular-toArrayFilter']);
+//
+// App.controller('CEnrollCtrl', function($scope, $http) {
+//   $http.get('https://whsatku.github.io/skecourses/combined.json')
+//     .then(function(res){
+//       console.log(res.data);
+//       $scope.courses = res.data;
+//     });
 // });
 
+angular.module('App', ['angular-toArrayFilter'])
+  .controller('HomeController', function ($http) {
+    var regist = this;
 
-// $(document).ready(function() {
-//   var courses = [];
-//
-//   $.getJSON('https://whsatku.github.io/skecourses/combined.json', function(data) {
-//     var output = "<ul>";
-//     var titleTag = "<div class=\"title\"><i class=\"dropdown icon\"></i>";
-//     var contentTag = "<div class=\"content\"><p class=\"transition hidden\">"
-//     for(var i in data) {
-//       output += titleTag + data[i].id + " - " + data[i].name.en + "</div>";
-//       output += contentTag + data[i].description.en + "<br><br>"
-//                 + checkUndefined(data[i].credit.self, data[i].credit.lab, data[i].credit.total, data[i].credit.lecture)  + "</div>";
-//
-//       courses.push({
-//         title: data[i].id + " - " +data[i].name.en
-//       });
-//     }
-//     output+="</ul>";
-//     document.getElementById("list").innerHTML = output;
-//     searching(courses);
-//   });
-//
-//   function checkUndefined(self, lab, total, lec) {
-//     var text = "( ";
-//     var keys = ["self", "lab", "total", "lecture"];
-//     var value = [self, lab, total, lec];
-//     for (var i in keys) {
-//       if(value[i] == undefined) {
-//         text += keys[i] + ": -";
-//       }
-//       else{
-//         text += keys[i] + ": " + value[i];
-//       }
-//       if(i != keys.length-1) {
-//         console.log(keys.length);
-//         text += " , ";
-//       }
-//     }
-//     text += " )";
-//     return text;
-//   }
-// });
+    $http.get('https://whsatku.github.io/skecourses/combined.json')
+      .success(function(res){
+        console.log(res);
+        regist.courses = res;
+      });
+  })
