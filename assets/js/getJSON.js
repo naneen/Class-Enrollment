fef81a2d79ae6d3ac25447faed9b9193bc5b19bb
naneen <neen_tta@hotmail.com>
2016-05-08 19:58:59 +0700

    Card is stackable

--- assets/js/getJSON.js
+++ assets/js/getJSON.js
@@ -6,58 +6,52 @@ App.controller('HomeController', function ($http) {
 
   // $http.get('https://whsatku.github.io/skecourses/combined.json')
   $http.get('https://whsatku.github.io/skecourses/list.json')
-    .success(function(res){
-      // console.log(res);
-      regist.courses = res;
-    });
+  .success(function(res){
+    regist.courses = res;
+  });
 })
 
 App.controller('ListController', function ($http) {
   var list = this;
 
-  // $http.get('https://whsatku.github.io/skecourses/combined.json')
   $http.get('https://whsatku.github.io/skecourses/combined.json')
-    .success(function(res){
-      list.detail = res;
-      console.log(list.detail);
-    });
+  .success(function(res){
+    list.detail = res;
+  });
 })
 
 App.controller('enrollController', function ($http, $stateParams) {
   var enroll = this;
-  enroll.courseID = $stateParams.courseID;
-  console.log(enroll.courseID);
-  // $http.get('https://whsatku.github.io/skecourses/combined.json')
-  //   .success(function(res){
-  //     enroll.detail = res;
-  //     console.log(enroll.detail);
-  //   });
+  var courseID = $stateParams.courseID;
+
+  $http.get('https://whsatku.github.io/skecourses/' + courseID + '.json')
+  .success(function(res){
+    enroll.course = res;
+  });
 })
 
+
 App.config(function($stateProvider, $urlRouterProvider) {
-  // For any unmatched url, redirect to /state1
   $urlRouterProvider.otherwise("/home");
-  //
-  // Now set up the states
   $stateProvider
-    .state('home', {
-      url: "/home",
-      templateUrl: "home.html"
-    })
-    // .state('state1.list', {
-    //   url: "/list",
-    //   templateUrl: "partials/state1.list.html",
-    //   controller: function($scope) {
-    //     $scope.items = ["A", "List", "Of", "Items"];
-    //   }
-    // })
-    .state('enroll', {
-      url: '/enroll/:courseID',
-      templateUrl: "enroll.html",
-      controller: "enrollController"
-    })
-    .state('list', {
-      url: "/list",
-      templateUrl: "list.html"
-    })
+  .state('home', {
+    url: "/home",
+    templateUrl: "home.html"
+  })
+  // .state('state1.list', {
+  //   url: "/list",
+  //   templateUrl: "partials/state1.list.html",
+  //   controller: function($scope) {
+  //     $scope.items = ["A", "List", "Of", "Items"];
+  //   }
+  // })
+  .state('enroll', {
+    url: '/enroll/:courseID',
+    templateUrl: "enroll.html",
+    controller: "enrollController"
+  })
+  .state('list', {
+    url: "/list",
+    templateUrl: "list.html"
+  })
 });
--- assets/js/jqueryApp.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('.button').on('click', function() {
-    $('.button').dimmer('show');
-  });
-});
