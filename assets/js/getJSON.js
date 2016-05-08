ca074ef424dd619b3188af749bd34f85f6aba76d
naneen <neen_tta@hotmail.com>
2016-05-08 23:17:06 +0700

    Could enroll & store enrolled courses in local array

--- assets/js/getJSON.js
+++ assets/js/getJSON.js
@@ -6,7 +6,7 @@ App.controller('HomeController', function ($http) {
 
   // $http.get('https://whsatku.github.io/skecourses/combined.json')
   $http.get('https://whsatku.github.io/skecourses/list.json')
-  .success(function(res){
+  .success(function(res) {
     regist.courses = res;
   });
 })
@@ -15,30 +15,50 @@ App.controller('ListController', function ($http) {
   var list = this;
 
   $http.get('https://whsatku.github.io/skecourses/combined.json')
-  .success(function(res){
+  .success(function(res) {
     list.detail = res;
   });
 })
 
-App.controller('EnrollController', function ($http, $stateParams) {
+App.controller('EnrollController', function ($http, $stateParams, MyCourseService) {
   var enroll = this;
   var courseID = $stateParams.courseID;
+  var c = [];
 
   $http.get('https://whsatku.github.io/skecourses/' + courseID + '.json')
-  .success(function(res){
+  .success(function(res) {
     enroll.course = res;
+    enroll.c = res;
   });
+
+  enroll.addCourse = function() {
+    MyCourseService.addCourse(enroll.c);
+  }
 })
 
-App.controller('DashboardController', function ($http, $stateParams) {
-  // var enroll = this;
-  // var courseID = $stateParams.courseID;
-  //
-  // $http.get('https://whsatku.github.io/skecourses/' + courseID + '.json')
-  // .success(function(res){
-  //   enroll.course = res;
-  // });
+App.controller('DashboardController', function ($http, MyCourseService) {
+  var dashb = this;
+  dashb.mycourses = MyCourseService.myCourses;
+  // dashb.mycourses = MyCourseService.getCourses();
+  console.log(dashb.mycourses);
 })
+
+App.service('MyCourseService', function() {
+  var myserv = this;
+  myserv.myCourses = [];
+
+  myserv.addCourse = function(courseID) {
+    console.log("service");
+    myserv.myCourses.push({
+      id: courseID
+    });
+    // console.log(myserv.myCourses);
+  };
+
+  // myserv.getCourses = function() {
+  //   return myserv.myCourses;
+  // };
+});
 
 App.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/home");
