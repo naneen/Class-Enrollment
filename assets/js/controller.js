9c53928745ba3f7ecb52f6073e2fde4ac93dcb84
naneen <neen_tta@hotmail.com>
2016-05-09 02:08:13 +0700

    Chang way how to push myCourses

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,14 +1,25 @@
 
 var App = angular.module('App', ['ui.router']);
 
-App.controller('HomeController', function ($http) {
-  var regist = this;
-
+App.controller('HomeController', function ($http, MyCourseService) {
+  var home = this;
+  var myCourses = MyCourseService.myCourses;
   // $http.get('https://whsatku.github.io/skecourses/combined.json')
   $http.get('https://whsatku.github.io/skecourses/list.json')
   .success(function(res) {
-    regist.courses = res;
+    home.courses = res;
+    // console.log(res);
   });
+
+  home.isEnrolled = function(courseID) {
+    // console.log("isEnrolled");
+    // console.log(myCourses);
+    // if(myCourses.indexOf(courseID) !== -1) {
+    //   console.log("EXIST!! : " + courseID);
+    //   return true;
+    // }
+      return false;
+  }
 })
 
 App.controller('ListController', function ($http) {
@@ -23,29 +34,25 @@ App.controller('ListController', function ($http) {
 App.controller('EnrollController', function ($http, $stateParams, MyCourseService) {
   var enroll = this;
   var courseID = $stateParams.courseID;
-  var c = [];
 
   $http.get('https://whsatku.github.io/skecourses/' + courseID + '.json')
   .success(function(res) {
     enroll.course = res;
-    enroll.c = res;
   });
 
   enroll.addCourse = function() {
-    // console.log(enroll.c);
-    MyCourseService.addCourse(enroll.c);
+    MyCourseService.addCourse(enroll.course);
   }
 
   enroll.dropCourse = function() {
-    // console.log(enroll.c);
-    MyCourseService.dropCourse(enroll.c);
+    // MyCourseService.dropCourse(enroll.course);
   }
 })
 
 App.controller('DashboardController', function ($http, MyCourseService) {
   var dashb = this;
   dashb.mycourses = MyCourseService.myCourses;
-  // console.log(dashb.mycourses);
+  console.log(dashb.mycourses);
 })
 
 App.service('MyCourseService', function() {
@@ -54,10 +61,10 @@ App.service('MyCourseService', function() {
 
   myserv.addCourse = function(c) {
     // console.log("service");
-    myserv.myCourses.push({
-      course : c
-    });
-    console.log(myserv.myCourses);
+    myserv.myCourses.push(
+      c
+    );
+    // console.log(myserv.myCourses);
   };
 
   myserv.dropCourse = function(courseID) {
